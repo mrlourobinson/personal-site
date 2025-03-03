8f6bf69cf167eaa9d8f6e411651622e0466baabe
mrlourobinson <hello@lourobinson.co.uk>
2025-03-03 18:10:13 +0000

    updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <div class="bio">
     <h2>Hey! 👋</h2>
 
-    <p>I'm Lou Robinson, a designer and visual journalist currently working as a data and graphics editor on <a href="https://edition.cnn.com/profiles/lou-robinson">the CNN Data and Graphics team</a>. I work on subjects ranging from climate to breaking news, using maps and data to tell important stories.</p>
+    <p>I'm Lou Robinson, a designer and visual journalist currently working as a data and graphics editor on the <a href="https://edition.cnn.com/profiles/lou-robinson">CNN Data and Graphics team</a>. I work on subjects ranging from climate to breaking news, using maps and data to tell important stories.</p>
     
     <p>I have worked as a designer in many sectors, but my work has always been about telling stories through visuals and data.</p>
 
